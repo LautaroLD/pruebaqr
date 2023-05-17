b46358d8b53315f3e4ea3bb1b912381b3a156960
LautaroLD <duranlautarogabriel@gmail.com>
2023-05-17 13:30:20 -0300

    change Pro Mode

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,7 @@ const App = (props) => {
 
     return (
         <div className="App">
-            <Html5QrcodePlugin
-                fps={10}
-                qrbox={250}
-                disableFlip={false}
-                qrCodeSuccessCallback={onNewScanResult}
-            />
+            <Html5QrcodePlugin />
         </div>
     );
 };
--- src/Html5QrcodePlugin.jsx
+++ src/Html5QrcodePlugin.jsx
@@ -1,50 +1,85 @@
-import { Html5QrcodeScanner } from 'html5-qrcode';
-import { useEffect } from 'react';
+// import { Html5QrcodeScanner } from 'html5-qrcode';
+// import { useEffect } from 'react';
 
-const qrcodeRegionId = "html5qr-code-full-region";
+// const qrcodeRegionId = "html5qr-code-full-region";
 
-// Creates the configuration object for Html5QrcodeScanner.
-const createConfig = (props) => {
-  let config = {};
-  if (props.fps) {
-    config.fps = props.fps;
-  }
-  if (props.qrbox) {
-    config.qrbox = props.qrbox;
-  }
-  if (props.aspectRatio) {
-    config.aspectRatio = props.aspectRatio;
-  }
-  if (props.disableFlip !== undefined) {
-    config.disableFlip = props.disableFlip;
-  }
-  return config;
-};
+// // Creates the configuration object for Html5QrcodeScanner.
+// const createConfig = (props) => {
+//   let config = {};
+//   if (props.fps) {
+//     config.fps = props.fps;
+//   }
+//   if (props.qrbox) {
+//     config.qrbox = props.qrbox;
+//   }
+//   if (props.aspectRatio) {
+//     config.aspectRatio = props.aspectRatio;
+//   }
+//   if (props.disableFlip !== undefined) {
+//     config.disableFlip = props.disableFlip;
+//   }
+//   return config;
+// };
 
-const Html5QrcodePlugin = (props) => {
+// const Html5QrcodePlugin = (props) => {
 
+//   useEffect(() => {
+//     // when component mounts
+//     const config = createConfig(props);
+//     const verbose = props.verbose === true;
+//     // Suceess callback is required.
+//     if (!(props.qrCodeSuccessCallback)) {
+//       throw "qrCodeSuccessCallback is required callback.";
+//     }
+//     const html5QrcodeScanner = new Html5QrcodeScanner(qrcodeRegionId, config, verbose);
+//     html5QrcodeScanner.render(props.qrCodeSuccessCallback, props.qrCodeErrorCallback);
+
+//     // cleanup function when component will unmount
+//     return () => {
+//       html5QrcodeScanner.clear().catch(error => {
+//         console.error("Failed to clear html5QrcodeScanner. ", error);
+//       });
+//     };
+//   }, []);
+
+//   return (
+//     <div id={qrcodeRegionId} />
+//   );
+// };
+
+// export default Html5QrcodePlugin;
+
+import { Html5Qrcode } from "html5-qrcode";
+import React, { useEffect, useState } from 'react'
+export default function Html5QrcodePlugin({ }) {
+  const [data, setdata] = useState()
+  // type Props = {}
+  // Html5Qrcode.getCameras().then(devices => {
+  //   /**
+  //    * devices would be an array of objects of type:
+  //    * { id: "id", label: "label" }
+  //    */
+  //   if (devices && devices.length) {
+  //     var cameraId = devices[0].id;
+  //     // .. use this to start scanning.
+  //   }
+  // }).catch(err => {
+  //   // handle err
+  // });
   useEffect(() => {
-    // when component mounts
-    const config = createConfig(props);
-    const verbose = props.verbose === true;
-    // Suceess callback is required.
-    if (!(props.qrCodeSuccessCallback)) {
-      throw "qrCodeSuccessCallback is required callback.";
-    }
-    const html5QrcodeScanner = new Html5QrcodeScanner(qrcodeRegionId, config, verbose);
-    html5QrcodeScanner.render(props.qrCodeSuccessCallback, props.qrCodeErrorCallback);
 
-    // cleanup function when component will unmount
-    return () => {
-      html5QrcodeScanner.clear().catch(error => {
-        console.error("Failed to clear html5QrcodeScanner. ", error);
-      });
+    const html5QrCode = new Html5Qrcode("reader");
+    const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+      /* handle success */
+      setdata(decodedResult)
     };
-  }, []);
-
+    const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+    html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
+  }, [])
   return (
-    <div id={qrcodeRegionId} />
-  );
-};
-
-export default Html5QrcodePlugin;
+    <>
+      <div id="reader" width="600px"></div>
+      <div>{data}</div>
+    </>
+  )
+}
